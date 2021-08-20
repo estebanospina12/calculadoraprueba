3ac4d81e6daf08ee682264462f9e7e4f80258f41
Esteban9810ospina <esteban.ospina@bvc.com.co>
2021-08-19 19:23:44 -0500

    primera subida commit

---
+++ src/main/java/com/calculadoraprueba/calculadoraprueba/CalculadorapruebaApplication.java
@@ -0,0 +1,13 @@
+package com.calculadoraprueba.calculadoraprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadorapruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadorapruebaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calculadoraprueba/calculadoraprueba/CalculadorapruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadoraprueba.calculadoraprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadorapruebaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
