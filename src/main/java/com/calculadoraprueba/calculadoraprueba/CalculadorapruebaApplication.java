f9e981a333df6e0450974a6d82ee8a22bbf8790c
estebanospina12 <84192756+Esteban9810ospina@users.noreply.github.com>
2021-08-27 14:55:13 -0500

    correcion

--- src/main/java/com/calculadoraprueba/calculadoraprueba/CalculadorapruebaApplication.java
+++ src/main/java/com/calculadoraprueba/calculadoraprueba/CalculadorapruebaApplication.java
@@ -9,19 +9,5 @@ public class CalculadorapruebaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalculadorapruebaApplication.class, args);
     }
-
-
-    public class A {
-        public static int counter = 0;
-    }
-
-    public class B {
-        private A first = new A();
-        private A second = new A();
-
-        public void runUpTheCount() {
-            first.counter ++;  // Noncompliant
-            second.counter ++;  // Noncompliant. A.counter is now 2, which is perhaps contrary to expectations
-        }
-    }
+    
 }
