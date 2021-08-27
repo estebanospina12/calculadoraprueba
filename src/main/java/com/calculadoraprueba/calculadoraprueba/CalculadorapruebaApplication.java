d483e12c00b1fbe52519b48b26e678317473bbc8
estebanospina12 <esteban.ospina@bvc.com.co>
2021-08-27 14:45:10 -0500

    prueba de sonarqube

--- src/main/java/com/calculadoraprueba/calculadoraprueba/CalculadorapruebaApplication.java
+++ src/main/java/com/calculadoraprueba/calculadoraprueba/CalculadorapruebaApplication.java
@@ -10,4 +10,18 @@ public class CalculadorapruebaApplication {
         SpringApplication.run(CalculadorapruebaApplication.class, args);
     }
 
+
+    public class A {
+        public static int counter = 0;
+    }
+
+    public class B {
+        private A first = new A();
+        private A second = new A();
+
+        public void runUpTheCount() {
+            first.counter ++;  // Noncompliant
+            second.counter ++;  // Noncompliant. A.counter is now 2, which is perhaps contrary to expectations
+        }
+    }
 }
